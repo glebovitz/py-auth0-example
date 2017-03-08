92985ab82b7e5eba5686ce3c320922536ecb5ada
Gregg Lebovitz <gregg@lebovitz.net>
2017-03-08 17:03:03 -0500

    update redis

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import os
 import yaml
 from functools import wraps
 from flask import Flask, request, jsonify, session, _app_ctx_stack
+from flask_session import Session
 from flask_cors import cross_origin
 
 def setServiceConfig(required, service, key, env):
@@ -16,6 +17,72 @@ def setServiceConfig(required, service, key, env):
         print("Service: '{0}' must include key: '{1}'".format(service, key))
         exit()
 
+
+class RedisConnection:
+
+    connection = None
+
+    def __init__(self, connect = False):
+        '''
+        Configure settings and initialize connection & db
+        '''
+        self.config = {
+            'redis' : {
+                'url' : None,
+                'database' : 0,
+                'host' : None,
+                'port' : None,
+                'username' : None,
+                'password' : None
+             }
+        }
+        self.r = None
+
+        # Set Mongo connection from with environment variables
+        from core.intelagent_config import app
+        self.config['redis']['url'] = app.config['services']['redis']['url']
+        if 'database' in app.config['services']['redis']:
+            self.config['redis']['database'] = app.config['services']['redis']['database']
+
+        url = self.config['redis']['url'].replace('redis://', '')
+        if '@' in url:
+            urlparts = url.split('@')
+            login = urlparts[0].split(':')
+            url = urlparts[1]
+            self.config['redis']['username'] = login[0] if self.config['redis']['username'] is None else None
+            self.config['redis']['password'] = login[1] if self.config['redis']['password'] is None else None
+        if ':' in url:
+            urlparts = url.split(':')
+            self.config['redis']['host'] = urlparts[0] if self.config['redis']['host'] is None else None
+            self.config['redis']['port'] = urlparts[1] if self.config['redis']['port'] is None else None
+        print ("host {0}, port {1}, user {2}, pass {3} ",
+            self.config['redis']['host'],
+            self.config['redis']['port'],
+            self.config['redis']['username'],
+            self.config['redis']['password'])
+        if connect:
+            self.connect()
+        RedisConnection.connection = self
+
+    def connect(self):
+        '''
+        Connect to Redis datastore connection.
+        '''
+        import redis
+        self.r = redis.Redis(
+            host     = self.config['redis']['host'],
+            port     = self.config['redis']['port'],
+            db       = self.config['redis']['database'],
+            password = self.config['redis']['password']
+        )
+
+        return self
+
+    def set_app_config_for_sessions(self, app_config_dict):
+        app_config_dict['SESSION_REDIS'] = self.r
+        return self
+
+
 app = Flask(__name__)
 app.config['services'] = {}
 
@@ -23,6 +90,13 @@ setServiceConfig(True,  'flask',     'ip',   'AUTH0_SERVICE_IP')
 setServiceConfig(True,  'flask',     'port',  'AUTH0_SERVICE_PORT')
 setServiceConfig(True,  'auth0',     'id',   'AUTH0_CLIENT_ID')
 setServiceConfig(True,  'auth0',     'key',  'AUTH0_CLIENT_SECRET')
+setServiceConfig(True,  'redis',     'url', 'REDIS_DB_URL')
+setServiceConfig(False, 'redis',     'database', 'REDIS_DB_DB')
+
+app.config['redis'] = RedisConnection(True).set_app_config_for_sessions(app.config)
+app.config['SESSION_TYPE'] = 'redis'
+app.config['PERMANENT_SESSION_LIFETIME'] = 60 * 60 * 24 * 60 # 2 months
+Session(app)
 
 # Format error response and append status code.
 def handle_error(error, status_code):
